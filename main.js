fbb393a1fde98595806895752525fdedace1bd58
MarVikt <v.m.podporogie@gmail.com>
2022-04-02 19:56:49 +0300

    multi-digit number

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 let arr = [];
+
+let getSum = function (question) {
+  let sum;
+  do {
+    sum = prompt(question);
+  } while (isNaN(parseFloat(sum)) || !isFinite(sum));
+  return sum;
+};
+
+alert('Введите 7 многозначных чисел');
+for (let i = 0; i < 7; i++) {
+  arr[i] = getSum('Многозначное число номер ' + (i+1));
+}
+
+for (let i = 0; i < 7; i++) {
+  if (arr[i][0] === '2' || arr[i][0] === '4') {
+   console.log(arr[i]); 
+  }
+}
+
